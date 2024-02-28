06d91960f44a9af1dc0e61dbdf6ce4a9242cfe22
Grace  Aku Nutifafa Awuma <122891783+Fafagrace@users.noreply.github.com>
2024-02-27 19:31:12 -0700

    In progresss

--- weather-app/src/components/TopButtons.jsx
+++ weather-app/src/components/TopButtons.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 function TopButtons() {
-  return (
-    <div>TopButtons</div>
-  )
+
+    const cities = [
+        {
+            id: 1,
+            title: 'London'
+        },
+        {
+            id: 2,
+            title: 'Sydney'
+        },
+        {
+            id: 3,
+            title: 'Tokyo'
+        },
+        {
+            id: 4,
+            title: 'Toronto'
+        },
+        {
+            id: 5,
+            title: 'Paris'
+        },
+    ]
+
+    return <div className='flex items-center justify-around my-6'>
+    {cities.map((city) => (
+        <button key={city.id} className='text-white text-lg font-medium'>{city.title}</button>
+    ))}
+    </div>
 }
 
-export default TopButtons
+export default TopButtons   
